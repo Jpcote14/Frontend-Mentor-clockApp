6cbe8639354b1661423b6a96c04f695b2e5e4849
Jpcote14 <jean-philippe_cote@hotmail.com>
2024-11-13 16:35:23 -0500

    fin de journee

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        texteGris: "#303030",
+        texteBlanc: "#FFFFFF",
+      },
+      backgroundImage: {
+        imageJourTable: "url('./assets/desktop/bg-image-daytime.jpg')",
+        imageJourMobile: "url('./assets/mobile/bg-image-daytime.jpg')",
+
+        imageNuitTable: "url('./assets/desktop/bg-image-nighttime.jpg')",
+        imageNuitMobile: "url('./assets/mobile/bg-image-nighttime.jpg')",
+      },
+    },
   },
   plugins: [],
 };
