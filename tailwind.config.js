51974819178e0035589ef3eddc3d6d920a7ec833
Jpcote14 <jean-philippe_cote@hotmail.com>
2024-11-14 14:19:24 -0500

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,20 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
       colors: {
         texteGris: "#303030",
         texteBlanc: "#FFFFFF",
       },
       backgroundImage: {
         imageJourTable: "url('../assets/desktop/bg-image-daytime.jpg')",
+        imageJourTablette: "url('../assets/desktop/bg-image-daytime.jpg')",
         imageJourMobile: "url('../assets/mobile/bg-image-daytime.jpg')",
 
         imageNuitTable: "url('../assets/desktop/bg-image-nighttime.jpg')",
+        imageNuitTablette: "url('../assets/desktop/bg-image-nighttime.jpg')",
         imageNuitMobile: "url('../assets/mobile/bg-image-nighttime.jpg')",
       },
     },
