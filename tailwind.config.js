b56c59c547464f2e46644f55df5d0c599c7d50f7
Jpcote14 <jean-philippe_cote@hotmail.com>
2024-11-14 13:33:44 -0500

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ module.exports = {
         texteBlanc: "#FFFFFF",
       },
       backgroundImage: {
-        imageJourTable: "url('./assets/desktop/bg-image-daytime.jpg')",
-        imageJourMobile: "url('./assets/mobile/bg-image-daytime.jpg')",
+        imageJourTable: "url('../assets/desktop/bg-image-daytime.jpg')",
+        imageJourMobile: "url('../assets/mobile/bg-image-daytime.jpg')",
 
-        imageNuitTable: "url('./assets/desktop/bg-image-nighttime.jpg')",
-        imageNuitMobile: "url('./assets/mobile/bg-image-nighttime.jpg')",
+        imageNuitTable: "url('../assets/desktop/bg-image-nighttime.jpg')",
+        imageNuitMobile: "url('../assets/mobile/bg-image-nighttime.jpg')",
       },
     },
   },
