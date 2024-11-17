869a98420826668a605b70a6b6d6bc9f3feca97f
Jpcote14 <jean-philippe_cote@hotmail.com>
2024-11-17 12:29:26 -0500

    push pour voir github page

---
+++ src/js/Bouton.js
@@ -0,0 +1,45 @@
+class Bouton {
+  // Attribues
+
+  constructor(idBouton, idChevron, idLabel) {
+    this.bouton = document.getElementById(idBouton);
+    this.chevron = document.getElementById(idChevron);
+    this.label = document.getElementById(idLabel);
+    this.expansion = document.getElementById("expansion");
+    this.quote = document.getElementById("quote");
+
+    this.ajouterListeners();
+  }
+
+  ajouterListeners() {
+    this.bouton.addEventListener("click", () => {
+      this.toggleChevron();
+      this.toggleLabel();
+      this.toggleExpansion();
+    });
+  }
+
+  toggleChevron() {
+    this.chevron.classList.toggle("rotate-180");
+    this.chevron.classList.toggle("mb-1");
+  }
+
+  toggleLabel() {
+    if (this.chevron.classList.contains("rotate-180")) {
+      this.setLabel("Less");
+    } else {
+      this.setLabel("More");
+    }
+  }
+
+  setLabel(nouveauLabel) {
+    this.label.textContent = nouveauLabel;
+  }
+
+  toggleExpansion() {
+    this.quote.classList.toggle("hidden");
+    this.expansion.classList.toggle("hidden");
+  }
+}
+
+const boutonMore = new Bouton("btnMore", "chevron", "btnMoreLibelle");
