2f27abf1a72bae197aa618b6fc2240d55d80d79e
Jpcote14 <jean-philippe_cote@hotmail.com>
2024-11-19 09:51:19 -0500

    push pour github page

--- src/js/programmingQuotes.js
+++ src/js/programmingQuotes.js
@@ -1,25 +1,20 @@
-// Sélection des éléments HTML
 const quote = document.getElementById("quote");
-const auteur = document.getElementById("author");
+const auteur = document.getElementById("auteur");
+const refreshIcon = document.getElementById("refreshIcon");
 
-// Fonction pour récupérer et afficher une citation aléatoire
 function fetchRandomQuote() {
   fetch("https://api.quotable.io/random")
     .then((response) => response.json())
     .then((data) => {
-      // Met à jour les éléments avec la citation et l'auteur
       quote.textContent = `"${data.content}"`;
       auteur.textContent = `- ${data.author}`;
     })
     .catch((error) => {
       console.error("Erreur lors de la récupération de la citation :", error);
-      quote.textContent = "Une erreur s'est produite.";
-      auteur.textContent = "";
     });
 }
 
-// Appeler une citation dès le chargement de la page
-fetchRandomQuote();
+refreshIcon.addEventListener("click", fetchRandomQuote);
 
-// Mettre à jour la citation toutes les 10 secondes
+fetchRandomQuote();
 setInterval(fetchRandomQuote, 10000);
